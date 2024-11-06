ecbf474f3dc008f5036babd16e3537b66aa71d8e
Lucas Stertz <lucas@lucasstertz.com>
2024-11-06 14:15:15 -0600

    Add initial Dependencies projects.

---
+++ Unity/com.chopsticks.dependencies/Assets/Scripts/Tests/Runtime/ExampleRuntimeTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Chopsticks.Dependencies.Tests
+{
+    public class ExampleRuntimeTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
