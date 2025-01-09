e33d280cceeff45bbb2bd908c1f33f845a2b1b37
Lucas Stertz <lucas@lucasstertz.com>
2025-01-08 22:00:45 -0600

    Switch to a substituted dependency container for testing.

--- Unity/com.chopsticks.dependencies/Assets/Scripts/Tests/Runtime/MonoContainerTests/MockMonoContainer.cs
+++ Unity/com.chopsticks.dependencies/Assets/Scripts/Tests/Runtime/MonoContainerTests/MockMonoContainer.cs
@@ -1,14 +1,38 @@
 ﻿using Chopsticks.Dependencies.Containers;
+using Chopsticks.Dependencies.Resolutions;
+using System;
+using System.Collections.Generic;
+
+using static MonoContainerTests.MockMonoContainer;
 
 namespace MonoContainerTests
 {
-    public class MockMonoContainer : MonoContainer<DependencyContainer>
+    public class MockMonoContainer : MonoContainer<MockDependencyContainer>
     {
-        public new DependencyContainer InternalContainer { get; set; } = new();
+        public abstract class MockDependencyContainer :
+            IDependencyContainer, IDependencyResolutionProvider
+        {
+            public abstract bool InheritParentDependencies { get; set; }
+            public abstract IDependencyResolutionProvider Parent { get; set; }
+
+            public abstract bool CanProvide(Type contract);
+            public abstract IDependencyContainer Deregister(DependencyRegistration registration);
+            public abstract DependencyResolution GetResolution(Type contract);
+            public abstract IEnumerable<DependencyResolution> GetResolutions(Type contract);
+            public abstract IEnumerable<DependencyResolution> GetResolutionsForDisposal();
+            public abstract IDependencyContainer Register(DependencySpecification specification, 
+                out DependencyRegistration registration);
+            public abstract bool Resolve(Type contract, out object implementation);
+            public abstract IEnumerable<object> ResolveAll(Type contract);
+        }
+
+
+        public new MockDependencyContainer InternalContainer { get; set; } = 
+            NSubstitute.Substitute.For<MockDependencyContainer>();
 
         public bool RegisteredNativeDependencies { get; set; }
 
-        protected override DependencyContainer SetUp() => 
+        protected override MockDependencyContainer SetUp() => 
             InternalContainer;
 
         protected override void RegisterNativeDependencies() => 
